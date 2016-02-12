ea8aa0a865475e444c6e19a263064b14517a7e26
rawaqasha <r.qasha@newcastle.ac.uk>
2016-02-12 11:43:18 +0000

    first commit

---
+++ scripts/block-deploy3.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e
+blueprint=$1
+block=$(ctx node name)
+CONTAINER_ID=$2
+BLOCK_NAME=$(ctx node properties block_name)
+BLOCK_URL=$3
+Input_file=$4
+
+# Start Timestamp
+STARTTIME=`date +%s.%N`
+
+ctx logger info "Deploying ${block} on ${CONTAINER_ID}"
+#-----------------------------------------#
+#----------- download the task -----------#
+ctx logger info "download ${block} block"
+
+[ ! -f ~/${blueprint}/tasks/${BLOCK_NAME} ] && wget -O ~/${blueprint}/tasks/${BLOCK_NAME}  ${BLOCK_URL} || ctx logger info "task already exists"
+
+#----------- download the task -----------#
+#-----------------------------------------#
+
+# End timestamp
+ENDTIME=`date +%s.%N`
+
+# Convert nanoseconds to milliseconds
+# crudely by taking first 3 decimal places
+TIMEDIFF=`echo "$ENDTIME - $STARTTIME" | bc | awk -F"." '{print $1"."substr($2,1,3)}'`
+echo "download $block in $CONTAINER_ID: $TIMEDIFF" * | sed 's/[ \t]/, /g' >> ~/list.csv
+
+# Start Timestamp
+STARTTIME=`date +%s.%N`
+
+#-----------------------------------------#
+#----------- Execute the task ------------#
+ctx logger info "Execute the block"
+sudo docker exec -it ${CONTAINER_ID} chmod 777 /root/${blueprint}/tasks/${BLOCK_NAME}
+sudo docker exec -it ${CONTAINER_ID} java -jar /root/${blueprint}/tasks/${BLOCK_NAME} ${blueprint} ${block} ${Input_file}
+#------------ Execute the task -----------#
+#-----------------------------------------#
+
+sudo docker ps -s >> ~/docker.csv
+# End timestamp
+ENDTIME=`date +%s.%N`
+
+# Convert nanoseconds to milliseconds
+# crudely by taking first 3 decimal places
+TIMEDIFF=`echo "$ENDTIME - $STARTTIME" | bc | awk -F"." '{print $1"."substr($2,1,3)}'`
+echo "execute $block in $CONTAINER_ID: $TIMEDIFF" * | sed 's/[ \t]/, /g' >> ~/list.csv
